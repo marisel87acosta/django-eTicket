2f182acfdfb56ec0e3f05f66a1d70c1eff5e6e04
marisel87acosta <marisel87acosta@gmail.com>
2015-09-11 10:31:34 -0300

    Create 0002_auto_20150701_1343.py

---
+++ 0002_auto_20150701_1343.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('apps', '0002_auto_20150701_0323'),
+    ]
+
+    operations = [
+        migrations.AlterModelTable(
+            name='categoria',
+            table='categoria',
+        ),
+    ]
